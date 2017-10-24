d5f513beea0ab0a2ffcddb3df143ca1851f49558
Jonathan Helmus <jjhelmus@gmail.com>
2017-10-24 13:26:34 -0500

    WIP: protobuf compiler with new compilers

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
-
-if [ "$(uname)" == "Darwin" ];
-then
-    # Switch to clang with C++11 ASAP.
-    export MACOSX_VERSION_MIN=10.7
-    export CC=clang
-    export CXX=clang++
-    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-    export LIBS="-lc++"
-elif [ "$(uname)" == "Linux" ];
-then
-    export CC=gcc
-    export CXX=g++
-fi
+set -ex
 
 ./autogen.sh
 ./configure --prefix="${PREFIX}" \
-            --with-pic \
-            --enable-shared \
-            --enable-static \
-	    CC="${CC}" \
-	    CXX="${CXX}" \
-	    CXXFLAGS="${CXXFLAGS} -O2" \
-	    LDFLAGS="${LDFLAGS}"
+            --build=${HOST}      \
+            --host=${HOST}       \
+            --with-pic           \
+            --enable-shared      \
+            CC_FOR_BUILD=${CC}   \
+            CXX_FOR_BUILD=${CXX} \
+            --enable-static
 make -j ${CPU_COUNT}
 make check -j ${CPU_COUNT}
 make install
