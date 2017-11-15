42a6cb43547e8efc15338aaded3aaf1dc223e997
Jonathan Helmus <jjhelmus@gmail.com>
2017-11-15 15:24:58 -0600

    sync with anaconda-recipes and update to 3.4.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
 
+cd python
+$PYTHON setup.py install --cpp_implementation --old-and-unmanageable
 
-if [ "$(uname)" == "Darwin" ];
-then
-    # Switch to clang with C++11 ASAP.
-    export MACOSX_VERSION_MIN=10.7
-    export CC=clang
-    export CXX=clang++
-    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-    export LIBS="-lc++"
-elif [ "$(uname)" == "Linux" ];
-then
-    export CC=gcc
-    export CXX=g++
-fi
-
-./autogen.sh
-./configure --prefix="${PREFIX}" \
-            --with-pic \
-            --enable-shared \
-            --enable-static \
-	    CC="${CC}" \
-	    CXX="${CXX}" \
-	    CXXFLAGS="${CXXFLAGS} -O2" \
-	    LDFLAGS="${LDFLAGS}"
-make -j ${CPU_COUNT}
-make check -j ${CPU_COUNT}
-make install
-(cd python && python setup.py install --cpp_implementation --single-version-externally-managed --record record.txt && cd ..)
+rm $SP_DIR/*-nspkg.pth
