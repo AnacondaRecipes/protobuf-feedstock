e2afbb3faed031bf67c9ab2d2ce9dd38296209b7
Jonathan Helmus <jjhelmus@gmail.com>
2018-04-06 11:23:30 -0500

    remove os-x fix, clang with C++11 used

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-
-if [ "$(uname)" == "Darwin" ];
-then
-    # Switch to clang with C++11 ASAP.
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-    export LIBS="-lc++"
-fi
-
 # Install python package now
 cd python
 
