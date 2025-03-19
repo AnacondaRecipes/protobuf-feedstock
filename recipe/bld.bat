6b30f695367acc3ac3fc0720074c8bee723673e7
Eric Lundby <Eric.Lundby@gmail.com>
2025-03-19 12:43:11 -0600

    Force newer clang. Remove hack.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -28,7 +28,6 @@ set CLANG_COMPILER_PATH=%Bazel_LLVM%/bin/clang.exe
 set BAZEL_VS="%VSINSTALLDIR%"
 set BAZEL_VC="%VSINSTALLDIR%/VC"
 
-:: _ALLOW_COMPILER_AND_STL_VERSION_MISMATCH can be removed when we have clang>=11.0
 bazel %OUTPUT_BASE% build ^
     --linkopt "/LIBPATH:%PREFIX%\libs" ^
     --action_env PYTHON_BIN_PATH=%PYTHON% ^
@@ -36,8 +35,6 @@ bazel %OUTPUT_BASE% build ^
     --host_cxxopt=/std:c++17 ^
     --compiler=clang-cl ^
     --verbose_failures ^
-    --copt=-D_ALLOW_COMPILER_AND_STL_VERSION_MISMATCH ^
-    --host_copt=-D_ALLOW_COMPILER_AND_STL_VERSION_MISMATCH ^
     //python/dist:binary_wheel ^
     --define=use_fast_cpp_protos=true
 if %ERRORLEVEL% neq 0 exit 1
